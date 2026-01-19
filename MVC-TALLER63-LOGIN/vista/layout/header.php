096eaf8588dbf8768bac635004ffdc627d280855
Joma <142707487+JoMa00@users.noreply.github.com>
2026-01-19 09:24:33 -0500

    Add files via upload

---
+++ MVC-TALLER63-LOGIN/vista/layout/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taller de Gestión de Alumnos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php?accion=inicio">Inicio</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarAlumnos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Alumnos</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarAlumnos">
+                            <li>
+                                <a class="dropdown-item" href="index.php?accion=crear">Crear Alumnos</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="index.php?accion=listar">Listar Alumnos</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarReporte" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reporte</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarAlumnos">
+                            <li>
+                                <a class="dropdown-item" href="index.php?accion=reportar">Generar reporte</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?accion=logout">Cerrar sesión</a>
+                    </li>
+
+                </ul>
+
+                <?php if (isset($_SESSION['usuario']['usuario'])): ?>
+                    <span class="navbar-text text-white ms-auto">
+                        <?= $_SESSION['usuario']['usuario'] ?>
+                    </span>
+                <?php endif; ?>
+            </div>
+        </div>
+    </nav>
+
+</body>
+
+</html>
